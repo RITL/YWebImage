b50b48ecfbddddef339104bad93c951c7595505e
岳文 <774655048@qq.com>
2016-03-20 10:00:10 +0800

    Initial Commit

---
+++ WebImageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebImageDemo
+//
+//  Created by YueWen on 16/3/20.
+//  Copyright © 2016年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
